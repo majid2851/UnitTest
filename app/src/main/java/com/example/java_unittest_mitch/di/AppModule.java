3c63ad07570edc0d35b9acb22d8bf31ca6c39577
majid2851 <majidbagheri2851@gmail.com>
2023-01-03 10:01:41 +0330

    UnitTest-Learning-byMitch=>part 10 is completed.

--- app/src/main/java/com/example/java_unittest_mitch/di/AppModule.java
+++ app/src/main/java/com/example/java_unittest_mitch/di/AppModule.java
@@ -1,9 +1,37 @@
 package com.example.java_unittest_mitch.di;
 
+import static com.example.java_unittest_mitch.persistence.NoteDatabase.DATABASE_NAME;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.example.java_unittest_mitch.persistence.NoteDao;
+import com.example.java_unittest_mitch.persistence.NoteDatabase;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public abstract class AppModule
 {
+    @Singleton
+    @Provides
+    static NoteDatabase provideNoteDatabase(Application application){
+        return Room.databaseBuilder(application,NoteDatabase.class,DATABASE_NAME).build();
+    }
+
+    @Singleton
+    @Provides
+    static NoteDao provideNoteDao(NoteDatabase noteDatabase)
+    {
+        return noteDatabase.getNoteDao();
+    }
+
+
+
+
 
 }
